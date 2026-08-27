e5fb2d9366633f5b28398690c2f0661e0f5a22c4
lucas <lucaspereirasantos8126@gmail.com>
2026-08-27 16:13:02 +0100

    Correção de um bug no `healthCheck.ts` e adaptação do ficheiro `vite.config.ts` para o deploy (GitHub Pages).

--- scripts/healthCheck.ts
+++ scripts/healthCheck.ts
@@ -2,10 +2,17 @@ export async function healthCheck(url: string) {
     const cloudflare_worker = "https://health-checker-api.8126lucas.workers.dev";
     try {
         const response = await fetch(`${cloudflare_worker}/?url=${encodeURIComponent(url)}`);
-        if (response.ok) {
-            return `✅ Online (Status: ${response.status})`;
+
+        if (!response.ok) {
+            return "❌ Offline or service error";
+        }
+
+        const data = await response.json();
+
+        if (data.ok) {
+            return `✅ Online (Status: ${data.status})`;
         } else {
-            return `⚠️ Issues found (Status: ${response.status})`;
+            return `⚠️ Issues found (Status: ${data.status})`;
         }
     } catch (error) {
         console.error("Service error:", error);
